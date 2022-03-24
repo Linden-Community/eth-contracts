61b2ae543267194118cd1c62695fe70b4a702df0
lijun <lj110119@aliyun.com>
2022-03-24 11:03:17 +0800

    update nft exchange

--- contracts/meta721.sol
+++ contracts/meta721.sol
@@ -8,7 +8,7 @@ contract METANFT is ERC721, Ownable {
     constructor() ERC721("METANFT", "MNFT") {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://";
+        return "http://dweb.lindensys.cn/ipns/k51qzi5uqu5divqrok0get110odatax0tv8uqh3forzp1zhoxy7ft7cua188gt/babaofan/";
     }
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
