1ed502f296b641ffb8368473689d6d4108c2520b
lijun <lj110119@aliyun.com>
2022-04-18 16:06:43 +0800

    update the tokenURI function.

--- nftExchange/contracts/METANFT.sol
+++ nftExchange/contracts/METANFT.sol
@@ -34,7 +34,19 @@ contract METANFT is
 
     function _baseURI() internal pure override returns (string memory) {
         return
+            "https://dweb.lindensys.cn/ipns/k51qzi5uqu5divqrok0get110odatax0tv8uqh3forzp1zhoxy7ft7cua188gt/babaofan/";
+    }
+
+    function _ipfsGateway() internal pure override returns (string memory) {
+        return
             "https://dweb.lindensys.cn/ipfs/";
+    }
+
+    function safeMint(
+        address to,
+        uint256 tokenId
+    ) public onlyRole(MINTER_ROLE) {
+        _safeMint(to, tokenId);
     }
 
     function safeMint(
@@ -42,7 +54,7 @@ contract METANFT is
         uint256 tokenId,
         string memory uri
     ) public onlyRole(MINTER_ROLE) {
-        _safeMint(to, tokenId);
+        safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
